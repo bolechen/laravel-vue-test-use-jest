3a251f08f08d8fd93ce279fac664bfd6efdbec80
Bole Chen <avenger@php.net>
2019-11-22 10:26:26 +0800

    feat: add jest.

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -3,7 +3,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Example Component</div>
+                    <div class="card-header">Example Component one</div>
 
                     <div class="card-body">
                         I'm an example component.
